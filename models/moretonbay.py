cf9be637423010942c10d07095cd5a4b5beb2e38
梦游 <126550160+MengYouXz@users.noreply.github.com>
2024-05-28 18:45:32 +0800

    1

--- models/moretonbay.py
+++ models/moretonbay.py
@@ -1,19 +1,24 @@
-"""
-Create your db model here
-"""
+
 from sqlalchemy import Column, String, Text, Integer, DateTime, func
 from .metadata_base import Base
 
+class MoretonBay(Base):
+    __tablename__ = 'moretonbay'
 
-
-class MoretonbayDAList(Base):
-    __tablename__ = 'moretonbay_da_list'
-    id = Column(Integer, primary_key=True)
-
+    id = Column(Integer, primary_key=True, autoincrement=True,)
+    application_id = Column(String(255), nullable=True,server_default=None)
     da_number = Column(String(255), nullable=False,unique=True)
-    lodged = Column(Integer, nullable=True)
-    description = Column(Text, nullable=True)
+    lodged_date = Column(Integer, nullable=True)
+    description = Column(Text, nullable=True,server_default=None)
+    site_name = Column(String(255), nullable=True,server_default=None)
+    statuses = Column(String(255), nullable=True,server_default=None)
+    properties = Column(Text, nullable=True,server_default=None)
+    parcel_lot_plan = Column(Text, nullable=True,server_default=None)
+    activity_level = Column(String(255), nullable=True,server_default=None)
+    applicant_name = Column(String(255), nullable=True,server_default=None)
+    documents = Column(Text, nullable=True,server_default=None)
 
     _tid = Column(String(255), nullable=False, server_default=None)
+
     created_at = Column(DateTime, server_default=func.now())
-    update_at = Column(DateTime, server_default=func.now(), onupdate=func.now())
+    updated_at = Column(DateTime, server_default=func.now(), onupdate=func.now())
